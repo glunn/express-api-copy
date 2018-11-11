f7cfe128294ee2e2f70ca6059b7593c50881a277
Abbie Glunn <aglunn@gmail.com>
2018-11-11 13:24:23 -0800

    Route change

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,15 @@
 const router = app => {
-    app.get('/', (request, response) => {
-        response.send({
-            message: 'Node.js and Express REST API'
-        });
+  app.get('/', function(request, response) {
+        var result = 'App is running'
+        response.send(result);
+    }).listen(app.get('port'), function() {
+        console.log('App is running, server is listening on port ', app.get('port'));
     });
+    // app.get('/', (request, response) => {
+    //     response.send({
+    //         message: 'Node.js and Express REST API'
+    //     });
+    // });
     // Display all users
     app.get('/users', (request, response) => {
         pool.query('SELECT * FROM users', (error, result) => {
